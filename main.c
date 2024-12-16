e4d8dd4cd74758178833b2f8f1250f1fc4f4c8d1
bochenwang <bochenwang@tencent.com>
2024-12-16 17:45:46 +0800

    chore: 更新编译配置和链接库路径

--- main.c
+++ main.c
@@ -1,7 +1,34 @@
-#include <stdio.h>
+#include <gtk/gtk.h>
 
-int main(void)
-{
-    printf("Hello, World!\n");
-    return 0;
+static void on_activate(GtkApplication *app, gpointer user_data) {
+    GtkWidget *window;
+    GtkWidget *button;
+
+    // 创建一个新的窗口
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Hello GTK");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
+
+    // 创建一个按钮
+    button = gtk_button_new_with_label("Click Me!");
+    g_signal_connect(button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_container_add(GTK_CONTAINER(window), button);
+
+    // 显示所有小部件
+    gtk_widget_show_all(window);
+}
+
+int main(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+
+    // 创建一个新的 GTK 应用程序
+    app = gtk_application_new("com.example.GtkApplication", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+
+    // 运行应用程序
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
 }
