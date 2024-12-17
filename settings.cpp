ee256ac3481c969dd11217b046af35c28f51c8ae
bochenwang <bochenwang@tencent.com>
2024-12-17 22:45:06 +0800

    chore: 更新构建配置和源文件
    移除settings.cpp文件，将settings.h移动到data目录下，并更新相关构建配置。

--- settings.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by bochen on 2024/12/17.
-//
-
-#include "settings.h"
-#include <iostream>
--- view/main.cpp
+++ view/main.cpp
@@ -1,9 +1,11 @@
 #include <_time.h>
 #include <iostream>
+#include "../data/settings.h"
 //
 // Created by bochen on 2024/12/17.
 //
 
 int main(int argc, char *argv[]) {
+    setttings &setting = setttings::get_instance();
     return 0;
 }
